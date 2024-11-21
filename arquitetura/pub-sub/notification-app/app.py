dfc6060479a3a163413b04a181b4d869008adf37
BrayanCordeiro <linsbvl73@gmail.com>
2024-11-20 21:25:04 -0300

    feat: atividade pub-sub

---
+++ arquitetura/pub-sub/notification-app/app.py
@@ -0,0 +1,44 @@
+from confluent_kafka import Consumer, KafkaError
+import json
+import logging
+import telegram
+
+ID = '-4573981332'
+KEY = '7695352564:AAEoXQzdTrMj-UHAbaCZSSkh9igo12X-PRU'
+
+def notificate(notification):
+    b = telegram.Bot(token=KEY)
+    b.send_message(chat_id=ID, text=notification)
+
+c = Consumer({
+    'bootstrap.servers': 'kafka1:19091,kafka2:19092,kafka3:19093',
+    'group.id': 'notification-group',
+    'client.id': 'client-1',
+    'enable.auto.commit': True,
+    'session.timeout.ms': 6000,
+    'default.topic.config': {'auto.offset.reset': 'smallest'}
+})
+
+c.subscribe(['notification'])
+
+try:
+    while True:
+        msg = c.poll(0.1)
+        if msg is None:
+            continue
+        elif not msg.error():
+            data = json.loads(msg.value())
+            filename = data.get('file')
+            logging.warning(f"READING {filename}")
+            notificate(json.dumps(data))
+            logging.warning(f"ENDING {filename}")
+        elif msg.error().code() == KafkaError._PARTITION_EOF:
+            logging.warning('End of partition reached {0}/{1}'
+                  .format(msg.topic(), msg.partition()))
+        else:
+            logging.error('Error occured: {0}'.format(msg.error().str()))
+
+except KeyboardInterrupt:
+    pass
+finally:
+    c.close()
--- arquitetura/pub-sub/rotate-app/app.py
+++ arquitetura/pub-sub/rotate-app/app.py
@@ -1,13 +1,19 @@
 from PIL import Image, ImageOps
 import os
-from confluent_kafka import Consumer, KafkaError
+from confluent_kafka import Consumer, KafkaError, Producer
 import json
 import logging
 from time import sleep
+from uuid import uuid4
 
 OUT_FOLDER = '/processed/rotate/'
 NEW = '_rotate'
 IN_FOLDER = "/appdata/static/uploads/"
+TOPIC = 'notification'
+
+configutation = {
+    'bootstrap.servers': 'kafka1:19091,kafka2:19092,kafka3:19093'  
+}
 
 def create_rotate(path_file):
     pathname, filename = os.path.split(path_file)
@@ -21,6 +27,35 @@ def create_rotate(path_file):
 
     name, ext = os.path.splitext(filename)
     transposed.save(output_folder + name + NEW + ext)
+    
+    m = {
+        "file": filename,
+        "status": "imagem rotacionada!"
+    }
+    send_message(m)
+    
+def delivery_report(e, data):
+    if e is not None:
+        print("Delivery failed for Message: {} : {}".format(data.key(), e))
+        return
+    print('Message: {} successfully produced to Topic: {} Partition: [{}] at offset {}'.format(
+        data.key(), data.topic(), data.partition(), data.offset()))
+    
+def send_message(m):
+    try:
+        p = Producer(configutation)
+        p.produce(
+            TOPIC,
+            key=str(uuid4()),
+            value=json.dumps(m),
+            callback=delivery_report
+        )
+
+        p.flush()
+    except Exception as e:
+        print(f"Error sending message: {e}")
+    finally:
+        p.flush()
 
 
 #sleep(30)
